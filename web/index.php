d47ef4d3ee168c91b48b4aea3225aa0991cc7349
Willi Eßer <willi.esser@unister.de>
2017-07-27 16:32:01 +0200

    [app] added lazy di and config loading

--- web/index.php
+++ web/index.php
@@ -15,9 +15,16 @@ $app->register(
     ]
 );
 
+// register a lazy DI container
 $app['service-container'] = function () {
     return new \Symfony\Component\DependencyInjection\ContainerBuilder();
 };
+
+// register config
+$app['config'] = \Symfony\Component\Yaml\Yaml::parse(
+    file_get_contents(getcwd() . '/config/app.yml'),
+    \Symfony\Component\Yaml\Yaml::PARSE_OBJECT_FOR_MAP
+);
 
 // application provider
 $app->mount('/', new \AmadeusService\Index\IndexProvider());
