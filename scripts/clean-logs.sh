def50abda0513a5670343cba7c17a5524c0cb5b0
Tibor Sari <tibor.sari@invia.de>
2017-11-08 09:08:22 +0100

    VAN-347 | (build) added bz2 extension for the extraction of local hosted composer packages

--- scripts/clean-logs.sh
+++ scripts/clean-logs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-echo "clean up nginx logs..."
+source $(dirname $0)/base.sh
+info "clean up nginx logs..."
 docker run -it --rm -v $(pwd):/app -w /app busybox rm var/logs/nginx/*.log -rf
-echo "clean up other logs..."
+info "clean up other logs..."
 docker run -it --rm -v $(pwd):/app -w /app busybox rm var/logs/*.log -rf
