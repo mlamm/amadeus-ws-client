6903f36fc00141e44aae81084491d7bdca3d4a00
Tibor Sari <tibor.sari@invia.de>
2017-11-08 09:30:02 +0100

    (fix) removed interactive shell command from docker run

--- scripts/clean-cache.sh
+++ scripts/clean-cache.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source $(dirname $0)/base.sh
 info "clean up jms serializer cache..."
-docker run -it --rm -v $(pwd):/app -w /app busybox rm var/cache/serializer/* -rf
+docker run --rm -v $(pwd):/app -w /app busybox rm var/cache/serializer/* -rf
 info "clean up profiler cache..."
-docker run -it --rm -v $(pwd):/app -w /app busybox rm var/cache/profiler/* -rf
+docker run --rm -v $(pwd):/app -w /app busybox rm var/cache/profiler/* -rf
 info "clean up twig cache..."
-docker run -it --rm -v $(pwd):/app -w /app busybox rm var/cache/twig/* -rf
+docker run --rm -v $(pwd):/app -w /app busybox rm var/cache/twig/* -rf
