e1ee8d7c994830e44e81083710fbb076960b1070
Tibor Sari <tibor.sari@invia.de>
2017-12-01 11:25:45 +0100

    VAN-524 (fix) | move dependencies from require-dev to require

--- web/index.php
+++ web/index.php
@@ -1,6 +1,4 @@
 <?php
-require_once __DIR__ . '/../vendor/perftools/xhgui-collector/external/header.php';
-
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
