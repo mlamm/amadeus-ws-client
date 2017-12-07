5e587e9d5d10216551f24acd72673ec71d25eae8
Marcus Winkler <marcus.winkler@invia.de>
2017-12-07 11:34:32 +0100

    clean config cache in build

--- scripts/clean-cache.sh
+++ scripts/clean-cache.sh
@@ -6,3 +6,5 @@ info "clean up profiler cache..."
 docker run --rm -v $(pwd):/app -w /app busybox rm var/cache/profiler/* -rf
 info "clean up twig cache..."
 docker run --rm -v $(pwd):/app -w /app busybox rm var/cache/twig/* -rf
+info "clean up config cache..."
+docker run --rm -v $(pwd):/app -w /app busybox rm var/cache/config -rf
