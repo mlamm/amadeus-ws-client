517d355fd7d324cf50bf328f8baefb36a9e49428
Etienne Tremel <etienne.tremel@container-solutions.com>
2018-02-06 14:20:51 +0100

    Run docker-compose with override instead

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,10 +9,10 @@ source $(dirname $0)/base.sh
 service_endpoint="http://localhost:80/health"
 
 info "Bringing services up with docker compose"
-docker-compose -f ./docker-compose.yml up -d
+docker-compose up -d
 
 # On error or when finished, shutdown containers
-trap 'gracefull_shutdown_docker_compose "-f ./docker-compose.yml"' ERR EXIT
+trap 'gracefull_shutdown_docker_compose' ERR EXIT
 
 # check readiness before running tests
 wait_for_endpoint $service_endpoint
