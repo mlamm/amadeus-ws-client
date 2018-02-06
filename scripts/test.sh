f6aa48aea4068e8bf2e080bcfd4b1bce529e334a
Etienne Tremel <etienne.tremel@container-solutions.com>
2018-02-06 17:09:47 +0100

    Another attempt?

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,10 +9,10 @@ source $(dirname $0)/base.sh
 service_endpoint="http://localhost:80/health"
 
 info "Bringing services up with docker compose"
-docker-compose up -d
+docker-compose -f docker-compose.test.yml up -d
 
 # On error or when finished, shutdown containers
-trap 'gracefull_shutdown_docker_compose' ERR EXIT
+trap 'gracefull_shutdown_docker_compose "-f docker-compose.test.yml"' ERR EXIT
 
 # check readiness before running tests
 wait_for_endpoint $service_endpoint
