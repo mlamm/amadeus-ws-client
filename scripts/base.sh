57af2db5552f76b88e6579c42a1fc7b25aa62a37
Tibor Sari <tibor.sari@invia.de>
2018-03-01 16:36:31 +0100

    (cleanup) scripts and docker-compose files

--- scripts/base.sh
+++ scripts/base.sh
@@ -6,24 +6,42 @@
 
 # install common scripts if doesn't exist and always pull latest
 function check_common_repo() {
+  local scriptsDirectory=${1:-$(dirname $0)}
+
   if [[ -n $GIT_PRIVATE_KEY_PATH ]]
   then
     export GIT_SSH_COMMAND="ssh -i ${GIT_PRIVATE_KEY_PATH}"
   fi
-  if [[ ! -d $(dirname $0)/common ]]
+  if [[ ! -d "$scriptsDirectory/common" ]]
   then
-    git clone ssh://git@stash.unister.lan:2200/flight/infrastructure.deployment-scripts.git $(dirname $0)/common
+    git clone ssh://git@stash.unister.lan:2200/flight/infrastructure.deployment-scripts.git "$scriptsDirectory/common"
   else
-    pushd $(dirname $0)/common
+    pushd "$scriptsDirectory/common"
     git pull
     popd
   fi
 }
 
-# Execute when file is sourced
-[[ $_ != $0 ]] && check_common_repo
+# find the base scripts directory and use the common dir of this dir instead of cloning it into each subdir where the base.sh is sourced
+function find_base_scripts_directory() {
+    local current=$0
+    while [ true ]
+    do
+        current=$(dirname $current)
+        if [[ -f "$current/base.sh" ]]
+        then
+            echo "$current"
+            exit 0
+        fi
+    done
+}
 
-source $(dirname $0)/common/base.sh
+scriptsDirectory=$(find_base_scripts_directory)
+
+# Execute when file is sourced
+[[ $_ != $0 ]] && check_common_repo "$scriptsDirectory"
+
+source "$scriptsDirectory/common/base.sh"
 
 build_image=amadeus-php-base-build
 nginx_image=${REGISTRY}/flight/invia/service/amadeus/nginx
--- scripts/test.sh
+++ scripts/test.sh
@@ -9,13 +9,13 @@ source $(dirname $0)/base.sh
 service_endpoint="http://localhost:80/health"
 
 info "Bringing services up with docker compose"
-docker-compose -f docker-compose.test.yml up -d
+docker-compose up -d
 
 # On error or when finished, shutdown containers
-trap 'gracefull_shutdown_docker_compose "-f docker-compose.test.yml"' ERR EXIT
+trap 'gracefull_shutdown_docker_compose' ERR EXIT
 
 # check readiness before running tests
 wait_for_endpoint $service_endpoint
 
 info "Running tests"
-./scripts/local/codecept.sh run -v --steps --no-interaction
+./scripts/local/codecept run -v --steps --no-interaction
