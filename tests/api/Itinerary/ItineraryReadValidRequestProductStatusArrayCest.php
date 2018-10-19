e32654099e869969914cd97a119c60ee8c1aaed3
Marcel Lamm <marcel.lamm@unister.de>
2018-10-19 12:39:07 +0200

    - fixing issue where related-product-status can be an array instead of string (noticed on booking with tix already issued)

---
+++ tests/api/Itinerary/ItineraryReadValidRequestProductStatusArrayCest.php
@@ -0,0 +1,71 @@
+<?php
+
+use Flight\Service\Amadeus\Amadeus\Client\MockSessionHandler;
+use GuzzleHttp\Psr7\Request;
+
+/**
+ * Test the GET /itinerary endpoint while using phiremock as the gds backend here.
+ *
+ * @author     Marcel Lamm <marcel.lamm@invia.de>
+ * @copyright  Copyright (c) 2018 Invia Flights Germany GmbH
+ */
+class ItineraryReadValidRequestProductStatusArrayCest
+{
+    /**
+     * @var \Helper\PhireHelper
+     */
+    private $phiremockHelper;
+
+    /**
+     * @param \Helper\PhireHelper $phiremockHelper
+     */
+    protected function _inject(\Helper\PhireHelper $phiremockHelper)
+    {
+        $this->phiremockHelper = $phiremockHelper;
+    }
+
+    /**
+     * @param ApiTester $I
+     */
+    public function _before(ApiTester $I)
+    {
+        $this->phiremockHelper->prep(
+            $I,
+            codecept_data_dir('fixtures/PNR_Retrieve_RelatedProductStatusArray.xml')
+        );
+    }
+
+    /**
+     * Test the GET /itinerary endpoint.
+     *
+     * @param ApiTester $I
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function testGetItin(ApiTester $I)
+    {
+        // http header, holding session and auth info
+        $httpHeader = [
+            'User-Agent' => 'Symfony BrowserKit',
+            'session' => file_get_contents(codecept_data_dir('requests/Price/valid-session-header.json')),
+            'authentication' => file_get_contents(codecept_data_dir('requests/Price/valid-auth-header.json'))
+        ];
+
+        $client = new GuzzleHttp\Client();
+        $request = new Request('GET', 'http://amadeus-nginx/itinerary/?recordLocator=QTDEOG', $httpHeader);
+
+        $response = $client->send($request);
+        \PHPUnit_Framework_Assert::assertSame(200, $response->getStatusCode());
+
+        /** @var \GuzzleHttp\Psr7\Stream $responseBody */
+        $responseBody = $response->getBody();
+        $responseBody = $responseBody->getContents();
+        /** @var \stdClass $responseBody */
+        $responseBody = \json_decode($responseBody);
+
+        \PHPUnit_Framework_Assert::assertEquals(json_decode(file_get_contents(
+            codecept_data_dir('fixtures/response/itinerary/correct-response-related-product.json')
+        )),
+            $responseBody
+        );
+    }
+}
